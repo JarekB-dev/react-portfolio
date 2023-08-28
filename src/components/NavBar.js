d8cab0b11eb0668aba4d095d0168a547053e8d56
jerrybonq <jerrybonq@gmail.com>
2023-08-28 17:31:37 +0100

    install bootstrap icons, add banner structure and typing effect.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import navIcon from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 
-export const Navbar = () => {
+export const MyNavbar = () => {
 	const [activeLink, setActiveLink] = useState("home");
 	const [scrolled, setScrolled] = useState(false);
 
