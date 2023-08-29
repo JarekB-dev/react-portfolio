a4a7aa524c4599020efee77e14e8a90da2053a4f
jerrybonq <jerrybonq@gmail.com>
2023-08-29 17:12:56 +0100

    finish styling skills section.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
-import Carousel from "react-bootstrap/Carousel";
+import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
